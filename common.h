99c368a05b4acfc90a331e8a7325e8e21ef905cd
Jari Korhonen <jtkorhonen@gmail.com>
2010-05-18 01:39:40 +0300

    Version is 0.4.3.

--- common.h
+++ common.h
@@ -6,7 +6,7 @@
 #include <QtCore>
 
 #define APPNAME                         "HgExplorer"
-#define APPVERSION                      "0.4.2"
+#define APPVERSION                      "0.4.3"
 #define MY_ICON_SIZE                    32
 #define REPOMENU_TITLE                  "Repository actions"
 #define WORKFOLDERMENU_TITLE            "Workfolder actions"
