ced17985f61965ed8cd6b5dea9cf2ebef6f6be95
Chris Cannam <cannam@all-day-breakfast.com>
2011-01-20 13:37:00 +0000

    * Rename the -osx.sh files to plain .sh, since we could use them elsewhere too.  Rely on bundled kdiff3 if it exists.

---
+++ easyhg-extdiff.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+p=`dirname $0`
+if [ $# -lt 2 ]; then 
+    echo Insufficient arguments: $@
+    exit 2
+fi
+while [ $# -gt 2 ]; do
+    shift
+done
+found=""
+for d in kdiff3 kdiff3.exe; do
+    if [ -x "$p/$d" ]; then
+	found=true
+	"$p/$d" "$1" "$2"
+	break
+    fi
+done
+if [ -z "$found" ]; then
+    od=/usr/bin/opendiff
+    if [ -x "$od" ]; then
+	"$od" "$1" "$2" | cat
+    fi
+fi
