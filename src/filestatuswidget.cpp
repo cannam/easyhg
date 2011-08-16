d9ff01e0c26ea67379c41aa0fb19db3e67fe1be7
Chris Cannam <cannam@all-day-breakfast.com>
2011-08-16 17:13:35 +0100

    Fix #229

--- src/filestatuswidget.cpp
+++ src/filestatuswidget.cpp
@@ -79,7 +79,7 @@ FileStatusWidget::FileStatusWidget(QWidget *parent) :
     m_descriptions[FileStates::Missing] = tr("These files are recorded in the version control, but absent from your working folder.<br>"
                                              "If you intended to delete them, select them and use Remove to tell the version control system about it.<br>"
                                              "If you deleted them by accident, select them and use Revert to restore their previous contents.");
-    m_descriptions[FileStates::InConflict] = tr("These files are unresolved following an incomplete merge.<br>Select a file and use Merge to try to resolve the merge again.");
+    m_descriptions[FileStates::InConflict] = tr("These files are unresolved following an incomplete merge.<br>Use Merge to try to resolve the merge again.");
     m_descriptions[FileStates::Unknown] = tr("These files are in your working folder but are not under version control.<br>"
 //                                             "Select a file and use Add to place it under version control or Ignore to remove it from this list.");
                                              "Select a file and use Add to place it under version control.");
