0afb7fa0c3945ac358f4ebcc886ab675c6d959dc
Chris Cannam <cannam@all-day-breakfast.com>
2012-02-27 17:14:03 +0000

    Update copyright dates to 2012

--- easyhg.py
+++ easyhg.py
@@ -4,8 +4,8 @@
 #
 #    Based on hgExplorer by Jari Korhonen
 #    Copyright (c) 2010 Jari Korhonen
-#    Copyright (c) 2010-2011 Chris Cannam
-#    Copyright (c) 2010-2011 Queen Mary, University of London
+#    Copyright (c) 2010-2012 Chris Cannam
+#    Copyright (c) 2010-2012 Queen Mary, University of London
 #    
 #    This program is free software; you can redistribute it and/or
 #    modify it under the terms of the GNU General Public License as
